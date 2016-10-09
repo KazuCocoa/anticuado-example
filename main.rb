8cd8463aa91ddb9c91a54e378ba55dc52f0c458d
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2016-10-09 15:46:17 +0900

    add android and varthage case

--- main.rb
+++ main.rb
@@ -2,7 +2,9 @@ require "git"
 require "anticuado"
 
 my_chat_ex_path = "project/my_chat_ex"
-web_socket_ios = "project/WebSocketDemoForiOS"
+cocapods_sample = "project/WebSocketDemoForiOS"
+carthage_sample = "project/MySandBoxApp"
+android_sample = "project/DroidTestHelper"
 
 def clone_or_pull(repo, path)
   Git.clone(repo, path)
@@ -11,8 +13,9 @@ rescue
 end
 
 clone_or_pull("https://github.com/KazuCocoa/my_chat_ex", my_chat_ex_path)
-clone_or_pull("https://github.com/KazuCocoa/WebSocketDemoForiOS", web_socket_ios)
-
+clone_or_pull("https://github.com/KazuCocoa/WebSocketDemoForiOS", cocapods_sample)
+clone_or_pull("https://github.com/KazuCocoa/MySandBoxApp", carthage_sample)
+clone_or_pull("https://github.com/KazuCocoa/DroidTestHelper", android_sample)
 
 p "=======Elixir========"
 
@@ -22,6 +25,19 @@ p out_ex
 
 p "=======iOS/CocoaPods========"
 
-outdated_ios = ::Anticuado::IOS::CocoaPods.outdated web_socket_ios
-out_ios = ::Anticuado::IOS::CocoaPods.format outdated_ios
-p out_ios
+outdated_ios_co = ::Anticuado::IOS::CocoaPods.outdated cocapods_sample
+out_ios_co = ::Anticuado::IOS::CocoaPods.format outdated_ios_co
+p out_ios_co
+
+p "=======iOS/Carthage========"
+
+outdated_ios_ca = ::Anticuado::IOS::Carthage.outdated carthage_sample
+out_ios_ca = ::Anticuado::IOS::CocoaPods.format outdated_ios_ca
+p out_ios_ca
+
+p "=======Android/Gradle========"
+
+::Anticuado::Java::Gradle.outdated android_sample, true
+json_android = ::Anticuado::Java::Gradle.parse_json "#{android_sample}/build/dependencyUpdates/report.json"
+out_android = ::Anticuado::Java::Gradle.format json_android
+p out_android
